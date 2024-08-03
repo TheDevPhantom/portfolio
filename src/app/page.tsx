a2565385eac0a674be7065750bbc3f0c5ce525ee
Adriaan Botha <adriaan.botha@typedev.co.za>
2024-08-04 01:54:50 +0200

    Adding links and twitter header image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import Image from 'next/image';
 import styles from './page.module.css';
+import { FaDribbble, FaGithub, FaLinkedin } from 'react-icons/fa6';
+import '../styles/index.scss';
 
 export default function Home() {
   return (
@@ -7,6 +9,30 @@ export default function Home() {
       <div className={styles.center}>
         <h2>Coming Soon</h2>
       </div>
+      <div className='links'>
+        <a
+          href='https://github.com/TheDevPhantom'
+          target='_blank'
+          rel='noreferrer'
+        >
+          <FaGithub />
+        </a>
+        <a
+          href='https://dribbble.com/janadriaanbotha'
+          target='_blank'
+          rel='noreferrer'
+        >
+          <FaDribbble />
+        </a>
+        <a
+          href='https://www.linkedin.com/in/adriaan-botha/'
+          target='_blank'
+          rel='noreferrer'
+        >
+          <FaLinkedin />
+        </a>
+      </div>
     </main>
   );
 }
+``;
